96b97c8abeb7139582240e53c19e83fbc9788c7e
Ivan Toro <ivan_93_bcn@hotmail.com>
2018-04-19 12:59:45 +0200

    Primer commit

---
+++ src/practicaavconv/PracticaAVconv.java
@@ -0,0 +1,40 @@
+package practicaavconv;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ivan Toro and David Muntal
+ */
+public class PracticaAVconv {
+    
+    //public static String INPUT_ZIP_FILE = "test.zip";
+    //public static String OUTPUT_FOLDER = "Cubo";
+
+    public static void main( String[] args )
+    {      
+        Scanner keyboard = new Scanner(System.in);
+        
+        boolean ok;
+        
+        System.out.println("----- COMPRESOR AVCONV -----");
+        
+        do{
+            System.out.print("\nNombre del archivo: ");
+
+            String file = keyboard.next();
+            String folder = file;
+
+            //Unzip and convert to JPG if necessary
+            Unzip unZip = new Unzip();
+            ok = unZip.unZipIt(file+".zip",folder);
+            
+            if(!ok){
+                System.out.println("\nIntroduzca un nombre correcto!");
+            }
+            
+        } while(!ok);
+    }
+    
+}
+
---
+++ src/practicaavconv/Unzip.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaavconv;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.io.OutputStreamWriter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+
+import java.io.FileReader;
+import java.io.Writer;
+import java.io.Reader;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
+
+/**
+ *
+ * @author spring
+ */
+
+public class Unzip
+{
+    List<String> fileList;
+
+    public boolean unZipIt(String zipFile, String outputFolder){
+
+        byte[] buffer = new byte[1024];
+
+        try{    		
+            //get the zip file content
+            ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+            //get the zipped file list entry
+            ZipEntry ze = zis.getNextEntry();
+
+            //create output directory if not exists
+            File folder = new File(outputFolder);
+            if(!folder.exists()){
+                    folder.mkdir();
+            }
+            
+             //unzip the files  
+            while(ze!=null){
+
+                String current = ze.getName();
+
+                String[] name = current.split("[.]");  
+                System.out.println(ze.getName());
+                String fileName = name[0]; 
+                String extension = name[1]; 
+
+                File newFile = new File(outputFolder + File.separator + current);
+                new File(newFile.getParent()).mkdirs();
+
+                FileOutputStream fos = new FileOutputStream(newFile);       
+
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+
+                fos.close();   
+
+              //if extension not jpg, convert
+
+              if (extension.equals("png") || extension.equals("gif") || extension.equals("bmp") || extension.equals("jpeg") || extension.equals("jpg")){
+
+
+                BufferedImage bufferedImage = ImageIO.read(new File(outputFolder + File.separator + current));
+
+                 // create a blank, RGB, same width and height, and a white background
+                 BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+                 newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
+
+                 // write to jpeg file
+                 ImageIO.write(newBufferedImage, "jpeg", new File(outputFolder + File.separator + fileName+".jpeg"));
+
+                 // delete the original file unziped
+                 newFile.delete();
+
+                 }
+
+                ze = zis.getNextEntry();
+            }
+    	
+            zis.closeEntry();
+            zis.close();
+
+            System.out.println("Done");
+            
+            return true;
+    		
+        }catch(IOException ex){
+           return false;
+        }
+    }    
+}
